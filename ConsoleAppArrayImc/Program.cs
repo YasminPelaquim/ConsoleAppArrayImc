f04edc7057cde19c38df8f5cd1ee3f78bf3ec2e0
Usuario <Usuario@CPD02-M06>
2024-05-07 10:46:25 -0300

    pronto

--- ConsoleAppArrayImc/Program.cs
+++ ConsoleAppArrayImc/Program.cs
@@ -14,7 +14,7 @@ namespace ConsoleAppArrayImc
             double[] peso = new double[5]; 
             double[] altura = new double[5];
             double[] imc = new double[5];
-            int cont = 0;
+            double cont = 0;
 
             for (int i = 0; i <= 4; i++)
             {
@@ -27,37 +27,50 @@ namespace ConsoleAppArrayImc
 
                 imc[i] = peso[i] / (altura[i] * altura[i]);
 
-                if (imc[i] <= 18.5)
+            }
+
+            
+
+            for (int j = 0; j <= 4; j++)
+            {
+
+                if (imc[j] <= 18.5)
                 {
                     Console.WriteLine("Abaixo do peso");
-                } else if (imc[i] <= 24.9)
+                }
+                else if (imc[j] <= 24.9)
                 {
                     Console.WriteLine("Peso ideal");
-                } else if (imc[i] <= 29.9)
+                }
+                else if (imc[j] <= 29.9)
                 {
                     Console.WriteLine("Levemente acima do peso");
                     cont++;
-                } else if (imc[i] <= 34.9)
+                }
+                else if (imc[j] <= 34.9)
                 {
                     Console.WriteLine("Obesidade grau I");
                     cont++;
-                } else if(imc[i] <= 39.9)
+                }
+                else if (imc[j] <= 39.9)
                 {
                     Console.WriteLine("Obesidade grau II (severa)");
                     cont++;
-                } else if (imc[i] > 40)
+                }
+                else if (imc[j] > 40)
                 {
                     Console.WriteLine("Obesidade grau III (mórbida)");
                     cont++;
                 }
 
-            }
-
-            for (int j = 0; j <= 4; j++)
-            {
                 Console.WriteLine("NOME: " + nome[j] + "  PESO: " + peso[j] + "  ALTURA: " + altura[j] + "  IMC: " + imc[j]);
-                Console.WriteLine("A porcentagem de pessoas que estam acima do peso é de " + cont / 100);
+                
             }
+            Console.WriteLine("O número de pessoas que estam acima do peso é de " + (cont/5) * 100 + "%");
+
+
+            Console.ReadKey();
+
         }
     }
 }
